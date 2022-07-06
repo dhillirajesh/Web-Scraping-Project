480b06072653b602646442d87f965214b162d88c
Kemburu Dhilli Rajesh <108202397+dhillirajesh@users.noreply.github.com>
2022-07-06 12:14:42 +0530

    Update extracting links.py

--- extracting links.py
+++ extracting links.py
@@ -1,4 +1,4 @@
-#importing necessary packages
+# importing necessary packages
 
 import requests
 import re
@@ -7,7 +7,7 @@ import pandas as pd
 import logging
 lis=[]
 
-#creating a recurcive function to get links and appending them to a csv file
+# creating a recurcive function to get links and appending them to a csv file
 
 def lin(url):
     
@@ -16,19 +16,19 @@ def lin(url):
     try:
         soup=BeautifulSoup(response.content,'html.parser')
         
-        for i in soup.find_all('a',href=True):                             #finds all the web elements with tag a
+        for i in soup.find_all('a',href=True):                             # finds all the web elements with tag a
             
-            b=i['href']                                                    #b is the link in the web element
+            b=i['href']                                                    # b is the link in the web element
             
-            if b not in lis and re.search(rf"^\b(?=\w){url}\b(?!\w)",b):   #checks if the link already existed in the list and the link starting with the base url
+            if b not in lis and re.search(rf"^\b(?=\w){url}\b(?!\w)",b):   # checks if the link already existed in the list and the link starting with the base url
                 
-                lis.append(b)                                              #if the link not in list it will be appended
+                lis.append(b)                                              # if the link not in list it will be appended
                 
-                df=pd.DataFrame([b])                                       #that link is turned into dataframe
+                df=pd.DataFrame([b])                                       # that link is turned into dataframe
                 
-                df.to_csv('links.csv',mode='a',index=False,header=False)   #append that link to csv file
+                df.to_csv('links.csv',mode='a',index=False,header=False)   # append that link to csv file
                 
-                lin(b)                                                     #again calling lin function
+                lin(b)                                                     # again calling lin function
     
     except Exception as Argument:
 
